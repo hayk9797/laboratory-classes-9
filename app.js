3ecf4ab611281961c243897c9d53ad8e5dfd45ed
Huseyin Kahraman <stormhero88@gmail.com>
2025-06-17 01:27:09 +0200

    MongoDB Server Entegrated

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 
 const bookRoutes = require("./routes/bookRoutes");
 const authorRoutes = require("./routes/authorRoutes");
+const { default: mongoose } = require("mongoose");
 
 dotenv.config();
 
@@ -13,8 +14,15 @@ const app = express();
 // app.use();
 app.use(express.json());
 
-app.use("/api/books", bookRoutes);
-app.use("/api/authors", authorRoutes);
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connection successfull'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
+// app.use("/api/books", bookRoutes);
+// app.use("/api/authors", authorRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
